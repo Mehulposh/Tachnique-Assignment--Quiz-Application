d35513471f2735a063af2f713a29bd3b19e9ae06
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-12-05 19:01:27 +0530

    added quiz attemt schema

---
+++ Backend/model/AttemptSchema.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const AttemptSchema = new mongoose.Schema({
+    quizId: { type: mongoose.Schema.Types.ObjectId, ref: 'Quiz', required: true },
+    quizTitle: { type: String, required: true },
+    userName: { type: String, default: 'Guest' },
+    answers: {
+        type: Map,
+        of: String,
+        required: true
+    },
+    score: { type: Number, required: true },
+    totalQuestions: { type: Number, required: true },
+    percentage: { type: Number, required: true },
+    timeTaken: { type: Number, default: 0 } // in seconds
+}, { timestamps: true  }
+)
